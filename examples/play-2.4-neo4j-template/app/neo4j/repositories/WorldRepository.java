295040321702bff04136950785c21222a3bb37d3
Sebastian Hardt <s.hardt@micromata.de>
2015-08-31 13:22:54 +0200

    Removed sample

--- examples/play-2.4-neo4j-template/app/neo4j/repositories/WorldRepository.java
+++
@@ -1,9 +0,0 @@
-package neo4j.repositories;
-
-import neo4j.models.World;
-import org.springframework.data.neo4j.repository.GraphRepository;
-
-
-
-public interface WorldRepository extends GraphRepository<World> {
-}
